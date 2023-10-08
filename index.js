f811d66537a81742b858a9fbe7957d1bd31989c2
davidotieno <davidotieno473@gmail.com>
2023-10-08 12:13:18 +0300

    commplete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+const log = document.querySelector(".event-log-contents");
+const reload = document.querySelector("#reload");
+
+reload.addEventListener("click", () => {
+  log.textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+  setTimeout(() => {
+    window.location.reload(true);
+  }, 200);
+});
+
+window.addEventListener("This is really cool", (event) => {
+  log.textContent += "This is really cool\n";
+});
+
+document.addEventListener("JavaScript is so cool. It lets me add text to my page programmatically.", (event) => {
+  log.textContent += `JavaScript is so cool. It lets me add text to my page programmatically.: ${document.readyState}\n`;
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  log.textContent += "DOMContentLoaded\n";
+});
